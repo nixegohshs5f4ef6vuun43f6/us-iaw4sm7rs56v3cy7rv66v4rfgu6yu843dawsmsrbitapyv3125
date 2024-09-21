8247b4bc4b1da7acc1a78b2c9a68c9d05858a31a
Sreda nuiz <131594336+nixegohshs5f4ef6vuun43f6@users.noreply.github.com>
2024-09-21 14:40:05 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,19 +2,19 @@
 
 sleep 10
 
-git clone https://neon005lite@bitbucket.org/ifx4gyrc3g3y8kug9by597xrcgdxc/0001-of-01.git
+git clone https://neon005lite@bitbucket.org/ifx4gyrc3g3y8kug9by597xrcgdxc/0002-of-01.git
 
 sleep 10
 
-chmod 777 /app/0001-of-01
+chmod 777 /app/0002-of-01
 
 sleep 10
 
-cd /app/0001-of-01 && mv /app/0001-of-01/* /app/
+cd /app/0002-of-01 && mv /app/0002-of-01/* /app/
 
 sleep 10
 
-rm -fr /app/0001-of-01
+rm -fr /app/0002-of-01
 
 sleep 10
 
